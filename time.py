aad438707da87021d314b841dcb074f566805770
wordsguy <74150238+wordsguy@users.noreply.github.com>
2021-03-22 23:00:36 +0200

    Delete time.py

--- time.py
+++
@@ -1,8 +0,0 @@
-import time
-
-start_time = time.time()
-print("launch time : %s seconds" % (time.time() - start_time))
-
-localtime = time.localtime()
-result = time.strftime("current time: %I:%M:%S %p", localtime)
-print(result)

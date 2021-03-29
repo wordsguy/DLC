9801573adc8a3c502d256e536cbbcebba3b1f275
wordsguy <74150238+wordsguy@users.noreply.github.com>
2021-03-29 20:59:42 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,28 @@
+ document.addEventListener('DOMContentLoaded', function() {
 
+                let correct = document.querySelector('.correct');
+                correct.addEventListener('click', function() {
+                    correct.style.backgroundColor = 'green';
+                    document.querySelector('#A1').innerHTML = 'Правильно';
+            });
+
+            let incorrects = document.querySelectorAll('.incorrect');
+            for (let i = 0; i < incorrects.length; i++) {
+                incorrects[i].addEventListener('click', function() {
+                    incorrects[i].style.backgroundColor = 'red';
+                    document.querySelector('#A1').innerHTML = 'Неправильно';
+
+                });
+            }
+    
+             document.querySelector('.check').addEventListener('click', function() {
+                 let input = document.querySelector('input');
+                 if (input.value === '17') {
+                     input.style.backgroundColor = 'green';
+                     document.querySelector('#A2').innerHTML = 'Правильно';
+                 } else {
+                     input.style.backgroundColor = 'red';
+                     document.querySelector('#A2').innerHTML = 'Неправильно';
+                 }
+             });
+           });

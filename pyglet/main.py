65be7721729c436a1e179a86509ff2c6ad3137c0
wordsguy <74150238+wordsguy@users.noreply.github.com>
2021-04-05 20:01:12 +0300

    Update main.py

--- pyglet/main.py
+++ pyglet/main.py
@@ -1,25 +1,42 @@
 import pyglet
 
-pyglet.options["shadow_window"] = False 
-pyglet.options["debug_gl"] = False 
+pyglet.options[
+	"shadow_window"
+	] = False 
+pyglet.options[
+	"debug_gl"
+	] = False 
 
 import pyglet.gl as gl
 
 class Window(pyglet.window.Window): 
-	def __init__(self, **args):
+	def __init__(
+		self,
+		**args
+	):
 		super().__init__(**args) 
 	
 	def on_draw(self):
 		gl.glClearColor(4, 4.5, 1.0, 7.0) 
 		self.clear() 
 	
-	def on_resize(self, width, height):
-		print("Resize ", (width, height))
+	def on_resize(
+		self, 
+		width, 
+		height
+		):
+			print("Resize ", (width, height))
 
 class win:
 	def __init__(self):
-		self.config = gl.Config(double_buffer = True, major_version = 3)
-		self.window = Window(config = self.config, width = 800, height = 600, caption ="Bulvė?", resizable = True, vsync = False)
+		self.config = gl.Config(
+			double_buffer = True, 
+			major_version = 3)
+		self.window = Window(
+			config = self.config, 
+			width = 800, height = 600, 
+			caption ="Bulvė?",
+			resizable = True, vsync = False)
 
 	def run(self):
 		pyglet.app.run()
